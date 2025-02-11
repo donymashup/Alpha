344497819c3d2b4582dd0ebef369ae1afedcf63b
dony0007 <donybaby0007@gmail.com>
2025-02-11 15:29:35 +0530

    login_ui_completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,14 @@
 
 //import 'package:alpha/common%20widgets/side_menu.dart';
+import 'package:alpha/common%20widgets/bottom_navigation_bar.dart';
 import 'package:alpha/common%20widgets/splash_screen.dart';
-import 'package:alpha/common%20widgets/welcome_screen.dart';
 import 'package:alpha/constants/app_constants.dart';
 import 'package:alpha/controllers/is_subscribed_controller.dart';
 import 'package:alpha/features/auth/screen/login.dart';
 import 'package:alpha/features/auth/screen/phone_number.dart';
-import 'package:alpha/features/auth/screen/otp_screen.dart';
-import 'package:alpha/features/auth/screen/registration.dart';
-import 'package:alpha/features/courses/screen/my_courses.dart';
-import 'package:alpha/features/home/screen/home_screen.dart';
-import 'package:alpha/common%20widgets/bottom_navigation_bar.dart';
-import 'package:alpha/features/course_detailed/screens/course_detail.dart';
 //import 'package:alpha/features/my_courses/widgets/rating.dart';
-import 'package:alpha/features/course_detailed/widgets/reviews.dart';
-import 'package:alpha/features/performance/student_performance_index.dart';
-import 'package:alpha/features/profile/my_profile.dart';
-import 'package:alpha/features/quiz/screen/quiz_info.dart';
-import 'package:alpha/features/timeline/calendar_schedule_page.dart';
-
-import 'package:alpha/common%20widgets/bottom_navigation_bar.dart';
-import 'package:alpha/features/home/screen/home_screen.dart';
 
 import 'package:flutter/material.dart';
-import 'package:alpha/constants/app_constants.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 
@@ -44,8 +29,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: AppConstant.primaryColor),
         useMaterial3: true,
       ),
+    //  home: CustomBottomNavigation()
 
-     home: login(),
+    // home: SplashScreen()
+
+     home: Login(),
+    // home: PhoneNumberVerificationPage()
 
     );
   }
