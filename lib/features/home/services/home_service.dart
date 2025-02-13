d8f7c15bc6c1f24e9366a48f41885b2b7be57fef
dony0007 <donybaby0007@gmail.com>
2025-02-13 14:26:13 +0530

    carousel_slider_images_api_completed

--- lib/features/home/services/home_service.dart
+++ lib/features/home/services/home_service.dart
@@ -1,68 +1,8 @@
-// import 'dart:convert';
-
-// import 'package:alpha/constants/utils.dart';
-// import 'package:alpha/models/available_courses_model.dart';
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// class HomeService {
-//   //get available courses
-//   Future<AvailableCoursesModel?> getAvailableCourses(
-//     {
-//       //required String userId,
-//     required BuildContext context,}
-//   ) async {
-//     try{
-//     // SharedPreferences prefs = await SharedPreferences.getInstance();
-//     // String? userId = prefs.getString('userId');
-//     var headers = {
-//   'Cookie': 'etcpro_ci_session=18sjlpb9r1jqtr68p77nfo77ds8qpd2a'
-// };
-// var request = http.MultipartRequest('POST', Uri.parse('https://demo.etcweb.in/api/getAvailableCourses'));
-// request.fields.addAll({
-//   'userid': '1',
-// });
-
-// request.headers.addAll(headers);
-
-// http.StreamedResponse response = await request.send();
-
-// if (response.statusCode == 200) {
-//   var responseBody = await response.stream.bytesToString();
-//   final jsonResponse = json.decode(responseBody);
-//   var availableCoursesModel = AvailableCoursesModel.fromJson(jsonResponse);
-
-//   if (availableCoursesModel.type== 'success') {
-//     showSnackbar(
-//       context, 'course fetch success');
-//       print("   data     ");
-//       print(availableCoursesModel.courses?[1].courseDetails?.name);
-//       return availableCoursesModel;
-//   }else{
-//     showSnackbar(
-//       context, 'course fetch failed');
-//   }
-// }
-// else {
-//   print("Failed to fetch courses: ${response.statusCode}");
-//   return null;
-// }
-//   }
-//   catch (e) {
-//     showSnackbar(
-//       context,
-//       e.toString(),
-//     );
-//   }
-// }
-// }
-
-
 
 import 'dart:convert';
 import 'package:alpha/constants/utils.dart';
 import 'package:alpha/models/available_courses_model.dart';
+import 'package:alpha/models/slider_images_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
@@ -127,4 +67,64 @@ class HomeService {
       return null;
     }
   }
+
+
+  Future<SliderImagesModel?> getSliderImages({
+    required BuildContext context,
+  }) async {
+    try {
+      var headers = {
+        'Cookie': 'etcpro_ci_session=18sjlpb9r1jqtr68p77nfo77ds8qpd2a'
+      };
+
+      var request = http.MultipartRequest(
+          'POST', Uri.parse('https://demo.etcweb.in/api/getSliderImages'));
+      request.fields.addAll({'userid': '1'});
+
+      request.headers.addAll(headers);
+
+      http.StreamedResponse response = await request.send();
+
+      if (response.statusCode == 200) {
+        var responseBody = await response.stream.bytesToString();
+        final jsonResponse = json.decode(responseBody);
+
+        // Validate jsonResponse structure
+        if (jsonResponse == null || jsonResponse.isEmpty) {
+          showSnackbar(context, 'Invalid response from server');
+          return null;
+        }
+
+        var sliderImagesModel = SliderImagesModel.fromJson(jsonResponse);
+
+        if (sliderImagesModel.type == 'success') {
+          showSnackbar(context, 'Slider images fetch success');
+
+          // Safely check if sliders list is not null or empty before accessing
+          if (sliderImagesModel.sliders != null &&
+              sliderImagesModel.sliders!.isNotEmpty) {
+            print("Data received: ${sliderImagesModel.sliders![0].title}");
+          } else {
+            print("No slider images available.");
+          }
+
+          return sliderImagesModel;
+        } else {
+          showSnackbar(context, 'Slider images fetch failed');
+          return null;
+        }
+      } else {
+        print("Failed to fetch slider images: ${response.statusCode}");
+        return null;
+      }
+    } catch (e) {
+      showSnackbar(context, "Error fetching slider images: $e");
+      print("Exception: $e");
+      return null;
+    }
+  }
+
+
+
 }
+
--- lib/features/home/widgets/course_list.dart
+++ lib/features/home/widgets/course_list.dart
@@ -1,129 +1,3 @@
-// import 'package:alpha/constants/app_constants.dart';
-// import 'package:alpha/features/course_detailed/screens/course_detail.dart';
-// import 'package:alpha/features/home/services/home_service.dart';
-// import 'package:alpha/models/available_courses_model.dart';
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-
-// class CourseLists extends StatelessWidget {
-//   const CourseLists({super.key});
-
-//  Future<AvailableCoursesModel?> fetchCourses(BuildContext context) async {
-//     HomeService homeService = HomeService();
-//     return await homeService.getAvailableCourses(context: context);
-// }
-
-// @override
-//   Widget build(BuildContext context) {
-//     return FutureBuilder<AvailableCoursesModel?>(
-//         future: fetchCourses(context),
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.waiting) {
-//             return Center(child: CircularProgressIndicator()); // Loading indicator
-//           } else if (snapshot.hasError) {
-//             return Center(child: Text('Error fetching courses'));
-//           } else if (!snapshot.hasData || snapshot.data == null || snapshot.data!.data.isEmpty) {
-//             return Center(child: Text('No courses available'));
-//           } else {
-//              final availableCourses = snapshot.data!;
-//     return ListView.builder(
-//       shrinkWrap: true,
-//       physics: const NeverScrollableScrollPhysics(),
-//       itemCount: availableCourses.courses!.length,
-//       itemBuilder: (context, index) {
-//         var course = availableCourses.courses![index];
-//         return GestureDetector(
-//           onTap: () {
-//            Navigator.push(
-//            context,
-//            MaterialPageRoute(builder: (context) => AnimatedTabBarScreen(isSubscribed: false,)),
-//           );
-//           },
-//           child: Card(
-//             margin: const EdgeInsets.all(8),
-//             shape: RoundedRectangleBorder(
-//               borderRadius: BorderRadius.circular(10),
-//             ),
-//             color: AppConstant.cardBackground,
-//             elevation: 5,
-//             child: Row(
-//               children: [
-//                 // Image section (Flexible with flex: 1)
-//                 Flexible(
-//                   flex: 1,
-//                   child: Padding(
-//                     padding: const EdgeInsets.all(8.0),
-//                     child: Hero(
-//                       tag: "imageCourse",
-//                       child: ClipRRect(
-//                         borderRadius: const BorderRadius.horizontal(
-//                           left: Radius.circular(10),
-//                         ),
-//                         child:course.courseDetails?.image != null
-//                           ? Image.network(
-//                               course.courseDetails!.image!,
-//                               width: 50,
-//                               height: 50,
-//                               fit: BoxFit.cover,
-//                               errorBuilder: (context, error, stackTrace) =>
-//                                   const Icon(Icons.broken_image),
-//                             )
-//                           : const Icon(Icons.image),
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-                
-//                 const SizedBox(width: 5),
-          
-//                 // Text and rating section (Flexible with flex: 2)
-//                 Flexible(
-//                   flex: 2,
-//                   child: Padding(
-//                     padding: const EdgeInsets.all(8.0),
-//                     child: Column(
-//                       mainAxisAlignment: MainAxisAlignment.start,
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         Text(
-//                           course.courseDetails?.name ?? "No Name",
-//                           style: const TextStyle(
-//                             fontSize: 16,
-//                             fontWeight: FontWeight.bold,
-//                           ),
-//                         ),
-//                         const SizedBox(height: 5),
-//                         Row(
-//                           children: [
-//                             const Icon(
-//                               Icons.star,
-//                               color: Colors.amber,
-//                               size: 18,
-//                             ),
-//                             const SizedBox(width: 5),
-//                             Text(
-//                               course.courseDetails?.name ?? "No Name",
-//                               style: const TextStyle(fontSize: 16),
-//                             ),
-//                           ],
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         );
-//       }, 
-//     );
-//           }
-//         }
-//     );
-//   }
-// }
-
-
 import 'package:alpha/constants/app_constants.dart';
 import 'package:alpha/features/course_detailed/screens/course_detail.dart';
 import 'package:alpha/features/home/services/home_service.dart';
@@ -141,110 +15,110 @@ class CourseLists extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<AvailableCoursesModel?>(
-        future: fetchCourses(context),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return const Center(child: Text('Error fetching courses'));
-          } else if (!snapshot.hasData || snapshot.data == null || snapshot.data!.courses == null || snapshot.data!.courses!.isEmpty) {
-            return const Center(child: Text('No courses available'));
-          } else {
-            final availableCourses = snapshot.data!;
-            return ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: availableCourses.courses?.length ?? 0, // Prevent null issue
-              itemBuilder: (context, index) {
-                var course = availableCourses.courses![index];
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => AnimatedTabBarScreen(isSubscribed: false),
-                      ),
-                    );
-                  },
-                  child: Card(
-                    margin: const EdgeInsets.all(8),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+      future: fetchCourses(context),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return const Center(child: Text('Error fetching courses'));
+        } else if (!snapshot.hasData ||
+            snapshot.data == null ||
+            snapshot.data!.courses == null ||
+            snapshot.data!.courses!.isEmpty) {
+          return const Center(child: Text('No courses available'));
+        } else {
+          final availableCourses = snapshot.data!;
+          return ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: availableCourses.courses?.length ?? 0,
+            itemBuilder: (context, index) {
+              var course = availableCourses.courses![index];
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          AnimatedTabBarScreen(isSubscribed: false),
                     ),
-                    color: AppConstant.cardBackground,
-                    elevation: 5,
-                    child: Row(
-                      children: [
-                        // Image section
-                        Flexible(
-                          flex: 2,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Hero(
-                              tag: "imageCourse", // Unique Hero tag
-                              child: ClipRRect(
-                                borderRadius: const BorderRadius.horizontal(
-                                  left: Radius.circular(10),
-                                ),
-                                child: course.courseDetails?.image != null
-                                    ? Image.network(
-                                        course.courseDetails!.image!,
-                                        width: 200,
-                                        height: 100,
-                                        fit: BoxFit.cover,
-                                        errorBuilder: (context, error, stackTrace) =>
-                                            const Icon(Icons.broken_image),
-                                      )
-                                    : const Icon(Icons.image),
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        const SizedBox(width: 5),
-
-                        // Course details section
-                        Flexible(
-                          flex: 3,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  course.courseDetails?.name ?? "No Name",
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                const SizedBox(height: 5),
-                                Row(
-                                  children: [
-                                    const Icon(
-                                      Icons.star,
-                                      color: Colors.amber,
-                                      size: 18,
-                                    ),
-                                    const SizedBox(width: 5),
-                                    Text(
-                                      "4.5", // Assuming rating exists
-                                      style: const TextStyle(fontSize: 16),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+                  );
+                },
+                child: Card(
+                  margin: const EdgeInsets.all(8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                );
-              },
-            );
-          }
-        });
+                  color: AppConstant.cardBackground,
+                  elevation: 5,
+                  child: Row(
+                    children: [
+                      // Image section with rounded corners on both sides
+                      Expanded(
+                        flex: 4,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10), // Rounded on both sides
+                          child: Hero(
+                            tag: "imageCourse",
+                            child: course.courseDetails?.image != null
+                                ? Image.network(
+                                    course.courseDetails!.image!,
+                                    width: double.infinity,
+                                    height: 100,
+                                    fit: BoxFit.cover,
+                                    errorBuilder: (context, error, stackTrace) =>
+                                        const Icon(Icons.broken_image),
+                                  )
+                                : const Icon(Icons.image, size: 80),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(width: 5),
+
+                      // Course details section
+                      Expanded(
+                        flex: 5,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                course.courseDetails?.name ?? "No Name",
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Row(
+                                children: [
+                                  const Icon(
+                                    Icons.star,
+                                    color: Colors.amber,
+                                    size: 18,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  Text(
+                                    "4.5", // Assuming rating exists
+                                    style: const TextStyle(fontSize: 16),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        }
+      },
+    );
   }
 }
