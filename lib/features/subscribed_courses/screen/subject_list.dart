b1fca9b4d21848dbb2e74c8d131b5198d42cf532
dony0007 <donybaby0007@gmail.com>
2025-03-06 17:27:39 +0530

    hero_image_error_resolved

--- lib/features/subscribed_courses/screen/subject_list.dart
+++ lib/features/subscribed_courses/screen/subject_list.dart
@@ -40,161 +40,159 @@ class _SubjectListState extends State<SubjectList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) {
-            return [
-               SliverAppBar(
-              expandedHeight: 200.0,
-              floating: false,
-              pinned: true,
-              backgroundColor: Colors.white,
-              flexibleSpace: LayoutBuilder(
-                builder: (context, constraints) {
-                  bool isCollapsed = constraints.maxHeight <= kToolbarHeight;
-
-                  return Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      // Background Image
-                      Image.asset(
-                        'assets/images/coverpage.jpg',
-                        fit: BoxFit.cover,
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+             SliverAppBar(
+            expandedHeight: 200.0,
+            floating: false,
+            pinned: true,
+            backgroundColor: Colors.white,
+            flexibleSpace: LayoutBuilder(
+              builder: (context, constraints) {
+                bool isCollapsed = constraints.maxHeight <= kToolbarHeight;
+      
+                return Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    // Background Image
+                    Image.asset(
+                      'assets/images/coverpage.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                    // Gradient Overlay (Dark at Bottom)
+                    Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          colors: [
+                            Colors.black.withOpacity(0.9), // Dark at the bottom
+                            Colors.transparent,            // Clear at the top
+                          ],
+                        ),
                       ),
-                      // Gradient Overlay (Dark at Bottom)
-                      Container(
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                            colors: [
-                              Colors.black.withOpacity(0.9), // Dark at the bottom
-                              Colors.transparent,            // Clear at the top
-                            ],
+                    ),
+                    // Title positioned at the bottom left
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 45, bottom: 10),
+                        child: Text(
+                          widget.className,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: isCollapsed ? 18 : 20,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                      // Title positioned at the bottom left
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 45, bottom: 10),
-                          child: Text(
-                            widget.className,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: isCollapsed ? 18 : 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              ),
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back_ios_new, size: 16, color: Colors.white),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
+                    ),
+                  ],
+                );
+              },
             ),
-            ];
-          },
-          body: FutureBuilder<SubjectListModel?>(
-            future: _subjectList,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text("Error: ${snapshot.error}"));
-              } else if (!snapshot.hasData ||
-                  snapshot.data!.subjects == null ||
-                  snapshot.data!.subjects!.isEmpty) {
-                return const Center(child: Text("No subjects found"));
-              }
-
-              return CustomScrollView(
-                slivers: [
-                  SliverList(
-                    delegate: SliverChildBuilderDelegate(
-                      (context, index) {
-                        final subject = snapshot.data!.subjects![index];
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ChapterList(
-                                  sublectImage: subject.subjectImage!,
-                                  subjectName: subject.subjectName!,
-                                  classId: widget.classId,
-                                  packageId: subject.packageid!,
-                                  batchId: widget.batchId,
-                                  subjectId: subject.subjectId!,
-                                ),
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios_new, size: 16, color: Colors.white),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          ];
+        },
+        body: FutureBuilder<SubjectListModel?>(
+          future: _subjectList,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text("Error: ${snapshot.error}"));
+            } else if (!snapshot.hasData ||
+                snapshot.data!.subjects == null ||
+                snapshot.data!.subjects!.isEmpty) {
+              return const Center(child: Text("No subjects found"));
+            }
+      
+            return CustomScrollView(
+              slivers: [
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      final subject = snapshot.data!.subjects![index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChapterList(
+                                sublectImage: subject.subjectImage!,
+                                subjectName: subject.subjectName!,
+                                classId: widget.classId,
+                                packageId: subject.packageid!,
+                                batchId: widget.batchId,
+                                subjectId: subject.subjectId!,
                               ),
-                            );
-                          },
-                          child: Card(
-                            margin: const EdgeInsets.all(10),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                              side: BorderSide(color: Colors.grey.shade300),
                             ),
-                            elevation: 4,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                children: [
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(8),
-                                    child: CachedNetworkImage(
-                                      imageUrl: subject.subjectImage ?? "",
+                          );
+                        },
+                        child: Card(
+                          margin: const EdgeInsets.all(10),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            side: BorderSide(color: Colors.grey.shade300),
+                          ),
+                          elevation: 4,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: CachedNetworkImage(
+                                    imageUrl: subject.subjectImage ?? "",
+                                    width: 110,
+                                    height: 80,
+                                    fit: BoxFit.cover,
+                                    placeholder: (context, url) => Shimmer.fromColors(
+                                      baseColor: Colors.grey[300]!,
+                                      highlightColor: Colors.grey[100]!,
+                                      child: Container(
+                                        width: 110,
+                                        height: 80,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    errorWidget: (context, url, error) => Image.asset(
+                                      'assets/images/onboarding1.jpg',
                                       width: 110,
                                       height: 80,
                                       fit: BoxFit.cover,
-                                      placeholder: (context, url) => Shimmer.fromColors(
-                                        baseColor: Colors.grey[300]!,
-                                        highlightColor: Colors.grey[100]!,
-                                        child: Container(
-                                          width: 110,
-                                          height: 80,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                      errorWidget: (context, url, error) => Image.asset(
-                                        'assets/images/onboarding1.jpg',
-                                        width: 110,
-                                        height: 80,
-                                        fit: BoxFit.cover,
-                                      ),
                                     ),
                                   ),
-                                  const SizedBox(width: 12),
-                                  Expanded(
-                                    child: Text(
-                                      subject.subjectName ?? "Subject",
-                                      style: const TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w600,
-                                      ),
+                                ),
+                                const SizedBox(width: 12),
+                                Expanded(
+                                  child: Text(
+                                    subject.subjectName ?? "Subject",
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
                                     ),
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
                           ),
-                        );
-                      },
-                      childCount: snapshot.data!.subjects!.length,
-                    ),
+                        ),
+                      );
+                    },
+                    childCount: snapshot.data!.subjects!.length,
                   ),
-                ],
-              );
-            },
-          ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
